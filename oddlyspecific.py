bb1cbf75261dfa2feab6b4cac0a70a9fd9b6c130
Blint324 <blint324gd@gmail.com>
2024-12-28 10:21:34 +0100

    added sub and made the prompts easier to understand

--- oddlyspecific.py
+++ oddlyspecific.py
@@ -2,11 +2,14 @@ number = 1
 import mpmath
 import webbrowser
 
-type = input("fib or pi or add or mult my nigga ")
+type = input("sub, fib or pi or add or mult ")
 type.lower()
 if type == "mult":
     multtimes = input("by how much bro?????????????? ")
-numbertimes = input("penis ")
+numbertimes = input("how many times ")
+if type == "sub":
+    subtimes = input("ok but by how much ")
+    number = input("whats da faken starten numba?")
 
 f = open("oddlyspecific.txt", "w")
 if type == "add":
@@ -32,5 +35,9 @@ elif type == "mult":
     for x in range(int(numbertimes)):
         f.write(f"{number}, ")
         number = number * int(multtimes)
+elif type == "sub":
+    for x in range(int(numbertimes)):
+        f.write(f"{number}, ")
+        number = int(number) - 1
 else:
     webbrowser.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
