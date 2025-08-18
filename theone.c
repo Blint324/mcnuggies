2ed84bc23923f325632972a021ce46b98f7207b3
blint324 <blint324gd@gmail.com>
2025-08-18 11:07:38 +0200

    added more messages

--- theone.c
+++ theone.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+
 #define KGRN "\x1B[32m"
 
 int main() {
-    char msg[] = "Knock knock, the one.";
+    srand(time(NULL));
+    int randnum = rand() % 3;
+    const char *messages[] = {
+        "Knock knock, the one.",
+        "Follow the white rabbit.",
+        "Wake up, Neo."
+    };
+    const char *msg = messages[randnum];
     for (int i = 0; msg[i] != '\0'; i++) {
         printf("%s%c", KGRN, msg[i]);
         fflush(stdout);
